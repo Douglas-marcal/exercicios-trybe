2f74b0139e360ac913ae42bd653d2efb62eec0f3
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-06 11:05:03 -0300

    add day one block 4

---
+++ modulo-01-Fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/ex1.js
@@ -0,0 +1,11 @@
+const myName = 'Douglas'
+const birthCity = 'Curitiba'
+let birthYear = 1999
+
+console.log(myName, birthCity, birthYear)
+birthYear = 2030
+console.log(birthYear)
+
+// birthCity = 'Paraná'
+// console.log(birthCity)
+// não é possível reatribuir novos valores à constantes
