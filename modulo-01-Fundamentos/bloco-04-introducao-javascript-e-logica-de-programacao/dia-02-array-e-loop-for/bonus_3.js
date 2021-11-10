06d46ade4d66ee66c0492527eaca05af2e830073
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-10 12:40:48 -0300

    bonus 3

---
+++ modulo-01-Fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/bonus_3.js
@@ -0,0 +1,18 @@
+// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+let lastNumber = numbers.length - 1
+
+for (let index = 0; index < numbers.length; index += 1) {
+  switch (index) {
+    case lastNumber:
+      newNumbers.push(numbers[lastNumber] * 2)
+      break;
+  }
+  if (index < lastNumber) {
+    let result = numbers[index] * numbers[index + 1]
+  newNumbers.push(result)
+  }
+}
+console.log(newNumbers)
