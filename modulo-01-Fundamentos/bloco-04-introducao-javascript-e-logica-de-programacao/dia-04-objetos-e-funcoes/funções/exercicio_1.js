e2ec37fceeaccc86f2fc927bf95bff06e5aaf763
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-11 14:01:43 -0300

    adiciona exercicio 1 funçoes

---
+++ modulo-01-Fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/funções/exercicio_1.js
@@ -0,0 +1,21 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function palindromo(string) {
+  let arr = []
+  for (let word of string) {
+    arr.push(word)
+  }
+  let revert = arr.reverse()
+  let compare = revert.join('')
+
+  if (string.toLowerCase() === compare.toLowerCase()) {
+    return true
+  } else {
+    return false
+  }
+  // console.log(compare)
+  // let compare = revert.join()
+  // console.log(compare)
+}
+
+console.log(palindromo('ArAra'))
