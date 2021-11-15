ff181036519788f613e59eec552fdb4030c3db79
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-15 14:37:08 -0300

    exercicio 7 - funcoes

--- modulo-01-Fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-introducao-logica-de-programacao-e-algoritmos/exercicio_5.js
+++ modulo-01-Fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-introducao-logica-de-programacao-e-algoritmos/exercicio_5.js
@@ -2,17 +2,16 @@ let n = 7
 let count = n
 
 for (let i = 0; i < n; i += 1) {
-  
   switch (i) {
-    case 0:
-      console.log(' '.repeat(count/2) + '*')
-      break;
-    case 1:
-      console.log(' '.repeat(count/2) + '*')
-      break;
-    case 2:
-      console.log(' '.repeat(count/2) + '*')
-      break;
+  case 0:
+    console.log(' '.repeat(count/2) + '*')
+    break;
+  case 1:
+    console.log(' '.repeat(count/2) + '*')
+    break;
+  case 2:
+    console.log(' '.repeat(count/2) + '*')
+    break;
   }
   count -= 2
 }
