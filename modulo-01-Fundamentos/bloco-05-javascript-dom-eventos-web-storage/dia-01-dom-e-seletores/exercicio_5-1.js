90c6dd0572ce7ae6bb65abbc087070cb50599f75
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-16 15:46:42 -0300

    exercicio 1

---
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-dom-e-seletores/exercicio_5-1.js
@@ -0,0 +1,5 @@
+const myText = document.querySelector('.center-content p');
+
+myText.innerText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quo necessitatibus explicabo ipsa enim? Hic veritatis, vel saepe libero alias consectetur debitis minima voluptates modi inventore totam explicabo eveniet cumque.'
+
+console.log(myText)
