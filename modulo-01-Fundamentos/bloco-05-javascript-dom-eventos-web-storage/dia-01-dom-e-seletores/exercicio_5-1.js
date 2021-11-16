8a9bdfdb891e53e73d0a0e3f3726a0dc7ede4013
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-16 15:54:12 -0300

    exercicio 4

--- modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-dom-e-seletores/exercicio_5-1.js
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-dom-e-seletores/exercicio_5-1.js
@@ -1,8 +1,11 @@
 const myText = document.querySelector('.center-content p');
 const bgColor = document.getElementsByClassName('main-content')[0];
 const bgColorCenter = document.querySelector('.main-content .center-content');
+const fixH1 = document.getElementsByClassName('title')[0];
+
 console.log(bgColorCenter);
 myText.innerText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quo necessitatibus explicabo ipsa enim? Hic veritatis, vel saepe libero alias consectetur debitis minima voluptates modi inventore totam explicabo eveniet cumque.';
 
 bgColor.style = 'background-color: rgb(76,164,109)';
 bgColorCenter.style = 'background-color: white';
+fixH1.innerText = 'Exercício 5.1 - JavaScript';
