2c6f2b07563cda7ec7c5e7a11e8d594b86fa0b3c
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-16 15:52:13 -0300

    exercicio 2 e 3

--- modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-dom-e-seletores/exercicio_5-1.js
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-dom-e-seletores/exercicio_5-1.js
@@ -1,5 +1,8 @@
 const myText = document.querySelector('.center-content p');
+const bgColor = document.getElementsByClassName('main-content')[0];
+const bgColorCenter = document.querySelector('.main-content .center-content');
+console.log(bgColorCenter);
+myText.innerText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quo necessitatibus explicabo ipsa enim? Hic veritatis, vel saepe libero alias consectetur debitis minima voluptates modi inventore totam explicabo eveniet cumque.';
 
-myText.innerText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quo necessitatibus explicabo ipsa enim? Hic veritatis, vel saepe libero alias consectetur debitis minima voluptates modi inventore totam explicabo eveniet cumque.'
-
-console.log(myText)
+bgColor.style = 'background-color: rgb(76,164,109)';
+bgColorCenter.style = 'background-color: white';
