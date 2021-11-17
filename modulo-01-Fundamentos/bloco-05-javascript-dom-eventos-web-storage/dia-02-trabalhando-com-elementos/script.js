487e3cf88b119a71cdccc09383fb25eeb925d13e
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-17 15:52:25 -0300

    exercise 3

--- modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -1,3 +1,4 @@
+// - 1
 const body = document.body;
 // console.log(body);
 
@@ -5,7 +6,13 @@ const h1 = document.createElement('h1');
 h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(h1);
 
+// - 2
 const main = document.createElement('main');
 main.className = 'main-content';
 // main.classList.add('main-content');
-body.appendChild(main)
+body.appendChild(main);
+
+// - 3
+const section = document.createElement('section');
+section.className = 'center-content';
+main.appendChild(section);
