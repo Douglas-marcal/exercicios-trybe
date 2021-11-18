bbbc73b9ad8b7e6405f73f9d3e7a06d095853171
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-18 12:58:53 -0300

    exercises 5.2

--- modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -104,31 +104,39 @@ main.appendChild(secondH3);
 main.appendChild(thirdH3);
 
 // --------------------------------------------------//
-// - 1
-// h1.classList.add('title');
+- 1
+h1.classList.add('title');
 
-// // - 2
-// firstH3.classList.add('description');
-// secondH3.classList.add('description');
-// thirdH3.classList.add('description');
+// - 2
+firstH3.classList.add('description');
+secondH3.classList.add('description');
+thirdH3.classList.add('description');
 
-// // - 3
-// let paiSection = document.querySelector('.left-content').parentNode;
-// paiSection.removeChild(section2);
+// - 3
+let paiSection = document.querySelector('.left-content').parentNode;
+paiSection.removeChild(section2);
 
-// // - 4
-// section3.style = 'margin-right: auto';
+// - 4
+section3.style = 'margin-right: auto';
 
-// // - 5
-// let greenColor = document.querySelector('.center-content');
-// greenColor.style = 'background-color: green';
+// - 5
+let greenColor = document.querySelector('.center-content');
+greenColor.style = 'background-color: green';
 
 // - 6
-// const deleteItems = document.querySelectorAll('ul');
-// for(let item of deleteItems) {
-//   if (item.innerText.includes('Nove') || item.innerText.includes('Dez')) {
-//     deleteItems.remove(item);
+let deleteItems = document.querySelectorAll('li');
+
+for (let index = 0; index < deleteItems.length; index += 1) {
+  if (deleteItems[index].innerText === 'Nove' || deleteItems[index].innerText === 'Dez') {
+    ul.removeChild(deleteItems[index]);
+  }
+}
+
+// const deleteItems = document.getElementsByTagName('li');
+// console.log(deleteItems);
+
+// for (let index = 0; index < deleteItems.length; index += 1) {
+//   if (deleteItems[index].innerText === 'Nove' || deleteItems[index].innerText === 'Dez') {
+//     deleteItems[index].remove();
 //   }
 // }
-
-// console.log(deleteItems)
