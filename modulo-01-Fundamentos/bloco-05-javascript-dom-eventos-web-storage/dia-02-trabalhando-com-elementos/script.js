c11d128f3d885267b113996b84ae2c43403c78b0
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-17 22:00:49 -0300

    reset exercise 8

--- modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -40,39 +40,57 @@ img.alt = 'Fotos aleatórias';
 section2.appendChild(img);
 
 // - 8
-const ul = document.createElement('ul');
-const li1 = document.createElement('li');
-const li2 = document.createElement('li');
-const li3 = document.createElement('li');
-const li4 = document.createElement('li');
-const li5 = document.createElement('li');
-const li6 = document.createElement('li');
-const li7 = document.createElement('li');
-const li8 = document.createElement('li');
-const li9 = document.createElement('li');
-const li10 = document.createElement('li');
+let orderedNumbers = [
+  'Um',
+  'Dois',
+  'Três',
+  'Quatro',
+  'Cinco',
+  'Seis',
+  'Sete',
+  'Oito',
+  'Nove',
+  'Dez'
+]
 
-section3.appendChild(ul);
-ul.appendChild(li1);
-li1.innerText = 'Um';
-ul.appendChild(li2);
-li2.innerText = 'Dois';
-ul.appendChild(li3);
-li3.innerText = 'Três';
-ul.appendChild(li4);
-li4.innerText = 'Quatro';
-ul.appendChild(li5);
-li5.innerText = 'Cinco';
-ul.appendChild(li6);
-li6.innerText = 'Seis';
-ul.appendChild(li7);
-li7.innerText = 'Sete';
-ul.appendChild(li8);
-li8.innerText = 'Oito';
-ul.appendChild(li9);
-li9.innerText = 'Nove';
-ul.appendChild(li10);
-li10.innerText = 'Dez';
+const ul = document.createElement('ul');
+console.log(ul)
+
+
+
+
+// const li1 = document.createElement('li');
+// const li2 = document.createElement('li');
+// const li3 = document.createElement('li');
+// const li4 = document.createElement('li');
+// const li5 = document.createElement('li');
+// const li6 = document.createElement('li');
+// const li7 = document.createElement('li');
+// const li8 = document.createElement('li');
+// const li9 = document.createElement('li');
+// const li10 = document.createElement('li');
+
+// section3.appendChild(ul);
+// ul.appendChild(li1);
+// li1.innerText = 'Um';
+// ul.appendChild(li2);
+// li2.innerText = 'Dois';
+// ul.appendChild(li3);
+// li3.innerText = 'Três';
+// ul.appendChild(li4);
+// li4.innerText = 'Quatro';
+// ul.appendChild(li5);
+// li5.innerText = 'Cinco';
+// ul.appendChild(li6);
+// li6.innerText = 'Seis';
+// ul.appendChild(li7);
+// li7.innerText = 'Sete';
+// ul.appendChild(li8);
+// li8.innerText = 'Oito';
+// ul.appendChild(li9);
+// li9.innerText = 'Nove';
+// ul.appendChild(li10);
+// li10.innerText = 'Dez';
 
 // - 9
 const firstH3 = document.createElement('h3');
@@ -101,3 +119,13 @@ section3.style = 'margin-right: auto';
 // - 5
 let greenColor = document.querySelector('.center-content');
 greenColor.style = 'background-color: green';
+
+// - 6
+// const deleteItems = document.querySelectorAll('ul');
+// for(let item of deleteItems) {
+//   if (item.innerText.includes('Nove') || item.innerText.includes('Dez')) {
+//     deleteItems.remove(item);
+//   }
+// }
+
+// console.log(deleteItems)
