789506dbed9c6caa72482fef27955abdb8d1b47d
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-19 13:57:02 -0300

    exercise 1

--- modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-03-eventos/horaDaPratica/script.js
+++ modulo-01-Fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-03-eventos/horaDaPratica/script.js
@@ -15,3 +15,21 @@ createDaysOfTheWeek();
 
 // - 1
 
+function addDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysUl = document.querySelector('#days');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const day = document.createElement('li');
+    day.classList.add('day');
+    day.innerText = dezDaysList[index];
+    if (day.innerText.includes(24) || day.innerText.includes(25) || day.innerText.includes(31)) {
+      day.classList.add('holiday');
+    }
+    if (day.innerText === '4' || day.innerText === '11' || day.innerText === '18' || day.innerText === '25') {
+      day.classList.add('friday');
+    }
+    daysUl.appendChild(day);
+  }
+}
+
+addDays()
