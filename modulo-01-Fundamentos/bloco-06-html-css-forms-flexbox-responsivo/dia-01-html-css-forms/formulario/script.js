4c5ebde972cac4c57962667126afbebdf86bffde
Douglas Marcal <douglasymarcal@gmail.com>
2021-11-25 20:24:47 -0300

    adiciona select com opcoes de estado

---
+++ modulo-01-Fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/formulario/script.js
@@ -0,0 +1,39 @@
+const brazil = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins',
+}
+
+const selectStates = document.querySelector('#select-states');
+
+for (let state in brazil) {
+  console.log(brazil[state]);
+  const createOption = document.createElement('option')
+  createOption.setAttribute('value', state)
+  createOption.textContent = brazil[state]
+  selectStates.appendChild(createOption)
+}
