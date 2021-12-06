5ed7e219335cb62202f53fb2b8e54f8583f6545f
Douglas Marcal <douglasymarcal@gmail.com>
2021-12-06 19:15:03 -0300

    exercise 1 - part I

--- modulo-01-Fundamentos/bloco-07-introducao-es6-testes-unitarios/dia-02-Objects/ex1-p1.js
+++ modulo-01-Fundamentos/bloco-07-introducao-es6-testes-unitarios/dia-02-Objects/ex1-p1.js
@@ -39,14 +39,19 @@ const customerInfo = (order) => {
   const name = order.name;
   const phoneNumber = order.phoneNumber;
   const address = Object.values(order.address);
-  console.log(`Olá ${delivery}, entrega para: ${name}, Telefone: ${phoneNumber}, R. ${address[0]}, Nº: ${address[1]}, AP: ${address[2]}`);
+  console.log(`Olá ${delivery}, entrega para: ${name}, Telefone: ${phoneNumber}, R. ${address[0]}, Nº: ${address[1]}, AP: ${address[2]}\n`);
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
-  
-
+  order.name = 'Luiz Silva';
+  order.payment = 50;
+  const name = order.name;
+  const payment = order.payment;
+  const pizzas = Object.keys(order.order.pizza);
+  const drink = order.order.drinks.coke.type;
+  console.log(`Olá ${name}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drink} é R$${payment},00.`);
 }
 
 orderModifier(order);
