7a7d0e3450384f63098cd7b3925d6b7804f1d08e
Douglas Marcal <douglasymarcal@gmail.com>
2022-01-12 19:50:32 -0300

    ex2 - bonus

--- modulo-01-Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-fetch-api-async-await/api.js
+++ modulo-01-Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-fetch-api-async-await/api.js
@@ -1,20 +1,27 @@
 const url = 'https://api.coincap.io/v2/assets';
-const container = document.querySelector('#coinsContainer')
+const secondURL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
+const container = document.querySelector('#coinsContainer');
+const myRequest = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+}
+
+const newFetch = async () => {
+  const brl = await fetch(secondURL, myRequest)
+  .then((response) => response.json())
+  .then(({ usd: { brl }}) => brl);
+  return brl;
+};
 
 const fetchAPI = () => {
-  const myRequest = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
-  }
-
   fetch(url, myRequest)
     .then((response) => response.json())
     .then(({ data }) => {
       const cryptoX = data.filter((element, index) => index < 10)
-      cryptoX.forEach(({ name, symbol, priceUsd }) => {
+      cryptoX.forEach( async ({ name, symbol, priceUsd }) => {
         const createLI = document.createElement('li');
-        const priceToNumber = +priceUsd;
-        createLI.textContent = `${name} ${symbol}: $ ${priceToNumber.toFixed(2)}`;
+        const priceToNumber = +priceUsd * await newFetch();
+        createLI.textContent = `${name} ${symbol}: R$ ${priceToNumber.toFixed(2)}`;
         container.appendChild(createLI);
       });
 
@@ -23,3 +30,4 @@ const fetchAPI = () => {
 };
 
 fetchAPI()
+
