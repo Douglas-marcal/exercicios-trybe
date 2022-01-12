f1b06fed9c906c0875c047cf793a52535a11e6a6
Douglas Marcal <douglasymarcal@gmail.com>
2022-01-12 11:42:27 -0300

    update

--- modulo-01-Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/agora_a_pratica/agora_a_pratica.js
+++ modulo-01-Fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/agora_a_pratica/agora_a_pratica.js
@@ -1,77 +1,77 @@
-// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-// const mars = {
-//   name: "Mars",
-//   distanceFromSun: {
-//     value: 227900000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// const venus = {
-//   name: "Venus",
-//   distanceFromSun: {
-//     value: 108200000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// const jupiter = {
-//   name: "Jupiter",
-//   distanceFromSun: {
-//     value: 778500000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// console.log(planetDistanceFromSun(mars)); // 1
-// console.log(planetDistanceFromSun(venus)); // 2
-// console.log(planetDistanceFromSun(jupiter)); // 3
+console.log(planetDistanceFromSun(mars)); // 1
+console.log(planetDistanceFromSun(venus)); // 2
+console.log(planetDistanceFromSun(jupiter)); // 3
 
-// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-// const mars = {
-//   name: "Mars",
-//   distanceFromSun: {
-//     value: 227900000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// const venus = {
-//   name: "Venus",
-//   distanceFromSun: {
-//     value: 108200000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// const jupiter = {
-//   name: "Jupiter",
-//   distanceFromSun: {
-//     value: 778500000,
-//     measurementUnit: "kilometers",
-//   },
-// };
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
 
-// console.log(planetDistanceFromSun(mars)); // 1
-// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // 3
-// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // 2
+console.log(planetDistanceFromSun(mars)); // 1
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // 3
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // 2
 
-// const getPlanet = () => {
-//   const mars = {
-//     name: "Mars",
-//     distanceFromSun: {
-//       value: 227900000,
-//       measurementUnit: "kilometers",
-//     },
-//   };
-//   console.log("Returned planet: ", mars);
-// };
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
 
-// setTimeout(() => getPlanet(), 4000);
+setTimeout(() => getPlanet(), 4000);
 
 // ------------------------6---------------------------------
 const messageDelay = () => Math.floor(Math.random() * 5000);
@@ -91,8 +91,6 @@ const greet = (temperature) =>
 
 const handleError = (errorReason) =>
   console.log(`Error getting temperature: ${errorReason}`);
-
-// crie a função sendMarsTemperature abaixo
 
 const sendMarsTemperature = (callback, error) => {
   if (Math.random() <= 0.6) callback(getMarsTemperature())
