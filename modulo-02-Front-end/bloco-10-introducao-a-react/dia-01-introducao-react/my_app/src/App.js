f16c26a27f3a1310d25394f4c1625be9ed31bfd9
Douglas Marcal <douglasymarcal@gmail.com>
2022-01-21 18:36:03 -0300

    Para fixar dia 10.1

---
+++ modulo-02-Front-end/bloco-10-introducao-a-react/dia-01-introducao-react/my_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Component from './Component';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Component/>
+    )
+  }
+}
+
+export default App;
---
+++ modulo-02-Front-end/bloco-10-introducao-a-react/dia-01-introducao-react/my_app/src/Component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Douglas Marçal</h1>
+        <p>Apenas um rapaz latino americano, sem dinheiro no banco</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
