9604a4ca4ca06482e486c9a458d15329447c332c
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-01 23:18:57 -0300

    exercise 12.1

--- modulo-02-Front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/exercise-dog/src/App.js
+++ modulo-02-Front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/exercise-dog/src/App.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isReady: false,
+      url: '',
+    };
+
+    this.requestNewDog = this.requestNewDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI() {
+    this.setState({ isReady: false }, () => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState(() => ({ url: data.message, isReady: true }));
+        });
+    });
+  }
+
+  requestNewDog() {
+    this.fetchAPI();
+  }
+
+  render() {
+    const { isReady, url } = this.state;
+    return (
+      <div>
+        {
+          isReady ? <img src={ url } alt="dog" /> : <h1>Loading</h1>
+        }
+        <br />
+        <button type="button" onClick={ this.requestNewDog }>Click</button>
+      </div>
+    );
+  }
 }
 
 export default App;
