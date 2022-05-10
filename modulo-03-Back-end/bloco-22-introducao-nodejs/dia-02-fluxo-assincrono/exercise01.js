94d34ffd84ad3b433313af9b3c15142cfe6cdab5
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-10 20:54:51 -0300

    fix: change file name

---
+++ modulo-03-Back-end/bloco-22-introducao-nodejs/dia-02-fluxo-assincrono/exercise01.js
@@ -0,0 +1,16 @@
+function returnPromise(number1, number2, number3) {
+  return new Promise((resolve, reject) => {
+    if (typeof number1 !== 'number' || typeof number2 !== 'number' || typeof number3 !== 'number') {
+      reject('Informe apenas números');
+    };
+
+    const result = (number1 + number2) * number3;
+    if (result < 50) {
+      reject('Valor muito baixo');
+    };
+
+    resolve(result);
+  })
+}
+
+module.exports = returnPromise;
