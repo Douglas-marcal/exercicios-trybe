6bf863047fc0ed17c20e29284972f1a77552d204
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-10 21:03:39 -0300

    fix: Create a async function

--- modulo-03-Back-end/bloco-22-introducao-nodejs/dia-02-fluxo-assincrono/exercise03.js
+++ modulo-03-Back-end/bloco-22-introducao-nodejs/dia-02-fluxo-assincrono/exercise03.js
@@ -8,9 +8,13 @@ const number1 = getRandomNumberBetween1And100();
 const number2 = getRandomNumberBetween1And100();
 const number3 = getRandomNumberBetween1And100();
 
-try {
-  const result = returnPromise(number1, number2, number3);
-  console.log(result);
-} catch (error) {
-  console.error(error)
+async function asyncFunction() {
+  try {
+    const result = await returnPromise(number1, number2, number3);
+    console.log(result);
+  } catch (error) {
+    console.error(error)
+  }
 }
+
+asyncFunction();
