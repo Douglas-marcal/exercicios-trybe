4590d39351761d18432feb333340260e61cd7846
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-12 15:29:48 -0300

    feat: exercise 2

--- modulo-03-Back-end/bloco-22-introducao-nodejs/dia-04-express/index.js
+++ modulo-03-Back-end/bloco-22-introducao-nodejs/dia-04-express/index.js
@@ -4,13 +4,29 @@ const app = express();
 
 const PORT = 3333;
 const OK = 200;
+const NOT_FOUND = 404;
 
 app.use(bodyParser.json());
 
-app.get('ping', (_, response) => {
-  const message = { message: 'pong'}
+app.get('/ping', (_, response) => {
+  const message = { message: 'pong'};
 
   response.status(OK).json(message);
 });
 
-app.listen(PORT, `Server running on port ${PORT}`);
+app.post('/hello', (request, response) => {
+  const { name } = request.body;
+
+  const message = { message: `Hello, ${name}` };
+
+  response.status(OK).json(message);
+});
+
+app.all('*', (request, response) => {
+  const { path } = request;
+  const message = { message: `${path} does'nt exists`};
+
+  response.status(NOT_FOUND).json(message);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
