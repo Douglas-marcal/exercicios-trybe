63d07da3728433b8b644e471b09a7482fd4ef533
Douglas Marcal <douglasymarcal@gmail.com>
2022-06-09 16:49:38 -0300

    feat: Exercise 07

--- modulo-03-Back-end/bloco-24-orm-autenticacao/exercises/src/controllers/BooksController.js
+++ modulo-03-Back-end/bloco-24-orm-autenticacao/exercises/src/controllers/BooksController.js
@@ -6,6 +6,17 @@ async function getAll(_request, response) {
   response.status(200).json(books);
 }
 
+async function getById(request, response) {
+  const { id } = request.params;
+
+  const book = await bookService.getById(id);
+
+  if (!book) return response.status(404).json({ message: 'Book not found' });
+
+  response.status(200).json(book);
+}
+
 module.exports = {
   getAll,
+  getById,
 };
--- modulo-03-Back-end/bloco-24-orm-autenticacao/exercises/src/routes/books.js
+++ modulo-03-Back-end/bloco-24-orm-autenticacao/exercises/src/routes/books.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const BooksController = require('../controllers/BooksController');
 
 routes.get('/', BooksController.getAll);
+routes.get('/:id', BooksController.getById);
 
 module.exports = routes;
