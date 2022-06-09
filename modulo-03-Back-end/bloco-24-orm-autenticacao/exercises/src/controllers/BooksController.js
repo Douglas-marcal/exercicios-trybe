df4b70a5f02e89a558b41a883350c3d7318d939c
Douglas Marcal <douglasymarcal@gmail.com>
2022-06-09 16:59:41 -0300

    feat: Exercise 10

--- modulo-03-Back-end/bloco-24-orm-autenticacao/exercises/src/controllers/BooksController.js
+++ modulo-03-Back-end/bloco-24-orm-autenticacao/exercises/src/controllers/BooksController.js
@@ -16,7 +16,25 @@ async function getById(request, response) {
   response.status(200).json(book);
 }
 
+async function addBook(request, response) {
+  const { title, author, pageQuantity } = request.body;
+
+  const bookInfo = { title, author, pageQuantity };
+
+  const insertedBook = await bookService.addBook(bookInfo);
+
+  const message = {
+    id: insertedBook.id,
+    title,
+    author,
+    pageQuantity,
+  };
+
+  response.status(201).json(message);
+}
+
 module.exports = {
   getAll,
   getById,
+  addBook,
 };
